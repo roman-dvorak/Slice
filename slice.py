76cc3ad70aa0418b67a6aabe0662f3e79fa0fd4a
Roman Dvořák <romandvorak@mlab.cz>
2020-12-17 19:14:50 +0100

    Update slice.py

--- slice.py
+++ slice.py
@@ -3,4 +3,16 @@
 import yaml
 import sys
 
+
 print(sys.argv)
+
+config_file = sys.argv[1]
+
+with open(config_file, 'r') as cfgf:
+    try:
+        cfg = yaml.safe_load(cfgf)
+        print(cfg)
+    except yaml.YAMLError as exc:
+        print(exc)
+        
+        
