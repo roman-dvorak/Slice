3613b55bc37d1978adbad14b3ae9b40cdff88e1a
Roman Dvořák <romandvorak@mlab.cz>
2020-12-18 00:18:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh -l
 
-echo "promenne"
-echo $(compgen -v | grep GIT)
-
-#cd /github/worspace
+cd /github/workflow
 
 FILES=$(git diff --name-only 467d60e67e44c2b6d691b033a186ecf6be12c592..eba99396edec2e2aa42fa25a36aee5d81fd3b0f7 | sort -u | grep '.stl\|.amf\|.STL\|.AMF')
 
