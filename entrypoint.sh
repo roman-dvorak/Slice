32e0a2cdf441c96af39ce43322671a53310f39d4
Roman Dvořák <romandvorak@mlab.cz>
2020-12-18 00:24:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 tree /github
 
-cd /github/workflow
+cd /github/workspace
 
 FILES=$(git diff --name-only 467d60e67e44c2b6d691b033a186ecf6be12c592..eba99396edec2e2aa42fa25a36aee5d81fd3b0f7 | sort -u | grep '.stl\|.amf\|.STL\|.AMF')
 
