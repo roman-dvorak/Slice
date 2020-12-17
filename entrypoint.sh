b09956537fcf2b2765b9c3c38953bc3439b6c238
Roman Dvořák <romandvorak@mlab.cz>
2020-12-18 00:39:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ cd /github/workspace
 
 git status
 git log
-FILES=$(git diff --name-only HEAD~1..HEAD | sort -u | grep '.stl\|.amf\|.STL\|.AMF')
+FILES=$(git diff --name-only HEAD~1 | sort -u | grep '.stl\|.amf\|.STL\|.AMF')
 
 for file in $FILES; do
   echo "Changed sliceable file:" $FILES;
