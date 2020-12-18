741b372bbfa6eebada97303e34b44c226ea1d6f1
Roman Dvořák <romandvorak@mlab.cz>
2020-12-18 02:32:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ CFG='slice.yml';
 WHITEPATH=$(cat slice.yml | shyaml get-values path);
 
 for file in $FILES; do
-  CONFIG=$(cat slice.yml | shyaml get-value default.config)
-  PRINTER=$(dirname $(cat slice.yml | shyaml get-value default.config))"/printer."$(cat slice.yml | shyaml get-value default.printer))".ini"
+  CONFIG=$(cat slice.yml | shyaml get-value default.config);
+  PRINTER=$(dirname $(cat slice.yml | shyaml get-value default.config))"/printer."$(cat slice.yml | shyaml get-value default.printer)".ini";
   
   echo "Changed sliceable file:" $file;
   # otestovat, jestli soubor je na whitelistu? 
