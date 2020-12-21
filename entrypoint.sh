152a1a516553b170fafb4f119d4a90e2586d31d2
Roman Dvořák <romandvorak@mlab.cz>
2020-12-21 16:42:52 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,14 +18,14 @@ WHITEPATH=$(cat slice.yml | shyaml get-values path);
 for file in $FILES; do
   CONFIG=$(cat slice.yml | shyaml get-value default.config);
   PRINTER=$(dirname $(cat slice.yml | shyaml get-value default.config))"/printer."$1".ini";
-  INI_OUT=$ROOT/out/ini/$(basename ${file%.*})
-  GCODE_OUT=$ROOT/out/gcode/$(basename ${file%.*})
+  INI_OUT=$ROOT/out/ini/$(basename ${file%.*}).ini
+  GCODE_OUT=$ROOT/out/gcode/$(basename ${file%.*}).gcode
   
   echo "Changed sliceable file:" $OUT
   #TODO: otestovat, jestli soubor je na whitelistu?
   
   echo "spojit soubory" $ROOT$CONFIG $ROOT$PRINTER "pro" $INIOUT
-  /home/merge_slic3r_conf.sh -m $ROOT$CONFIG -p $ROOT$PRINTER -o $INIOUT
+  /home/merge_slic3r_conf.sh -m $ROOT$CONFIG -p $ROOT$PRINTER -o $INI_OUT
   echo "konfigurace spojeny"
   echo "gcode:" $GCODE_OUT;
   echo "ini" $INI_OUT;
