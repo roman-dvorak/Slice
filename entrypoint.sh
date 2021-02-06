5ac7a303736ac9295cf39ef51f2a08289f54b32b
Roman Dvořák <romandvorak@mlab.cz>
2021-02-06 17:21:55 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@
 echo "PARAMETRY>" $@
 echo "Spoustim s nastavenim" $1
 
+( set -o posix ; set ) | less
+
 cd /github/workspace
 ROOT=/github/workspace
 
@@ -15,20 +17,20 @@ mkdir out
 mkdir out/gcode
 mkdir out/ini
 
-#FILES=$(git diff --name-only HEAD~1 | sort -u | grep '.stl\|.amf\|.STL\|.AMF');
+#FILES=$(git diff --name-only HEAD~1 | sort -u | grep '.scad\|.SCAD');
 #FILES="/hw/CAD/stl/RPM_case.stl";
-FILES=$(find . | sort -u | grep '\.stl\|\.amf\|\.STL\|\.AMF')
+FILES=$(find . | sort -u | grep '\.SCAD\|\.scad')
 
-CFG='slice.yml';
-WHITEPATH=$(cat slice.yml | shyaml get-values path);
+#CFG='slice.yml';
+#WHITEPATH=$(cat render.yml | shyaml get-values path);
 
 for file in $FILES; do
-  CONFIG=$(cat slice.yml | shyaml get-value default.config);
-  PRINTER=$(dirname $(cat slice.yml | shyaml get-value default.config))"/printer."$1".ini";
-  INI_OUT=$ROOT/out/ini/$(basename ${file%.*}).ini
-  GCODE_OUT=$ROOT/out/gcode/$(basename ${file%.*}).gcode
+#   CONFIG=$(cat slice.yml | shyaml get-value default.config);
+#   PRINTER=$(dirname $(cat slice.yml | shyaml get-value default.config))"/printer."$1".ini";
+#   INI_OUT=$ROOT/out/ini/$(basename ${file%.*}).ini
+#   GCODE_OUT=$ROOT/out/gcode/$(basename ${file%.*}).gcode
   
-  echo "Changed sliceable file:" $file
+  echo "Changed render:" $file
   
   # zjistit, jestli STL je na whitelistu
 #   if echo $file| grep -zw "/hw" > /dev/null; then 
@@ -39,20 +41,20 @@ for file in $FILES; do
 #   fi
   
   # Zakladni .ini
-  MERGE_PARAM="-p "$ROOT$CONFIG
-  ## Existuje konfigurace pro tiskornu??
-  if [ -f "$ROOT$$PRINTER" ]; then
-    MERGE_PARAM=$MERGE_PARAM" -p "$ROOT$PRINTER
-  fi
-  MERGE_PARAM=$MERGE_PARAM" -o "$INI_OUT
-  
-  echo "spojit soubory"
-  /home/merge_slic3r_conf.sh $MERGE_PARAM
-  echo "konfigurace spojeny"
-  
-  echo "gcode:" $GCODE_OUT;
-  slic3r --no-gui --load $INI_OUT --output $GCODE_OUT $file
-  #echo "" > out/gcode/$file.gcode
+#   MERGE_PARAM="-p "$ROOT$CONFIG
+#   ## Existuje konfigurace pro tiskornu??
+#   if [ -f "$ROOT$$PRINTER" ]; then
+#     MERGE_PARAM=$MERGE_PARAM" -p "$ROOT$PRINTER
+#   fi
+#   MERGE_PARAM=$MERGE_PARAM" -o "$INI_OUT
+
+#   echo "spojit soubory"
+#   /home/merge_slic3r_conf.sh $MERGE_PARAM
+#   echo "konfigurace spojeny"
+
+#   echo "gcode:" $GCODE_OUT;
+#   slic3r --no-gui --load $INI_OUT --output $GCODE_OUT $file
+#   #echo "" > out/gcode/$file.gcode
   #echo "" > out/ini/$file.ini
-  
+   
 done
